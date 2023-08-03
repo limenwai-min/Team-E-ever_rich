f2e3f2ef138af40eea991bef031be281fa622143
yenka <yenkang@outlook.sg>
2023-08-03 17:37:30 +0800

    edited grammar

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import cash_on_hand,  overheads, profit_loss
 
 from pathlib import Path
 
-# create list containing the functions
+# created a list containing the functions
 summary = [overheads.overhead_function(),
            cash_on_hand.coh_function(),
            profit_loss.profitloss_function()]
 
 
-# create new text file under same folder
+# created a new text file under the same folder
 new_file = Path.cwd()/'summary_report.txt'
 new_file.touch()
 
-# write the contents of summary into the text file
+# wrote the contents of summary into the text file
 with new_file.open(mode='w') as report:
     for line in summary:
         report.write(line)
